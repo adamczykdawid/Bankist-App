2c3280e2bbf4ca51c34ce580b4642e40145535bc
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-16 20:52:19 +0100

    added loan request functionality

--- script.js
+++ script.js
@@ -168,6 +168,19 @@ btnTransfer.addEventListener('click', function (e) {
     receiverAccount.movements.push(amount);
     updateUI(currentAccount);
   }
+  console.log(currentAccount);
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
 });
 
 btnClose.addEventListener('click', function (e) {
@@ -188,6 +201,8 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
+console.log(accounts, account4, currentAccount);
 
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
