ac7c9676d5dc3be7ab4077b27f369f76c26b7de2
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-19 14:48:14 +0100

    working with numeric separators

--- bankist-numbers/script.js
+++ bankist-numbers/script.js
@@ -335,3 +335,16 @@ btnSort.addEventListener('click', function (e) {
 //     if (i % 3 === 0) row.style.backgroundColor = 'blue';
 //   });
 // });
+//numeric separators
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+console.log(Number('230000'));
+console.log(Number('230_000'));
